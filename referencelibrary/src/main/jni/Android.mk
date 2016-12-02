581d6b43396c0c35c67da13cc1e5bd1d74c3873a
André Kaustell <andre.kaustell@controlthings.fi>
2016-12-02 14:43:38 +0200

    Added -Wno-unused-function.

--- referencelibrary/src/main/jni/Android.mk
+++ referencelibrary/src/main/jni/Android.mk
@@ -12,7 +12,7 @@ LOCAL_MODULE    := mist
 LOCAL_SRC_FILES := wish_bridge_jni.c jni_utils.c wish_core_api.c mist_node_api_jni.c wish_app_deps/wish_platform.c wish_app_deps/wish_debug.c wish_app_deps/wish_utils.c $(foreach sdir,$(WISH_MODULES),$(wildcard $(sdir)/*.c))
 LOCAL_C_INCLUDES := deps/mbedtls-2.1.2/include deps/cBSON wish deps/ed25519/src wish_rpc wish_app wish_app_deps port/unix deps/bson/ deps/uthash/include mist
 LOCAL_LDLIBS := -llog
-LOCAL_CFLAGS := -O2 -Wall -Wno-pointer-sign -Werror -fvisibility=hidden -Wno-unused-variable
+LOCAL_CFLAGS := -O2 -Wall -Wno-pointer-sign -Werror -fvisibility=hidden -Wno-unused-variable -Wno-unused-function
 
 #Put each function in own section, so that linker can discard unused code
 LOCAL_CFLAGS += -ffunction-sections -fdata-sections 
