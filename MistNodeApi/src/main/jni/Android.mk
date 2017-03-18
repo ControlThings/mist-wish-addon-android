8b6ce138c5dfdf0d32209019e24f342e85dc58ce
André Kaustell <andre.kaustell@controlthings.fi>
2017-03-18 21:54:51 +0200

    Updated MistNodeApi to use mist-c99 instead of old mist-esp.

--- MistNodeApi/src/main/jni/Android.mk
+++ MistNodeApi/src/main/jni/Android.mk
@@ -5,14 +5,14 @@ include $(CLEAR_VARS)
 # Here we give our module name and source file(s)
 
 
-WISH_MODULES = deps/mbedtls-2.1.2/library deps/cBSON wish_app wish_rpc deps/ed25519/src deps/bson mist
+WISH_MODULES = mist-c99/deps/mbedtls-2.1.2/library mist-c99/deps/cbson/src mist-c99/wish_app mist-c99/deps/wish-rpc-c99/src mist-c99/deps/ed25519/src mist-c99/deps/bson mist-c99/mist
 
 WISH_SRC := 
 LOCAL_MODULE    := mist
-LOCAL_SRC_FILES := wish_bridge_jni.c jni_utils.c wish_core_api.c mist_node_api_jni.c wish_app_deps/wish_platform.c wish_app_deps/wish_debug.c wish_app_deps/wish_utils.c wish_app_deps/wish_fs.c filesystem.c $(foreach sdir,$(WISH_MODULES),$(wildcard $(sdir)/*.c))
-LOCAL_C_INCLUDES := deps/mbedtls-2.1.2/include deps/cBSON wish deps/ed25519/src wish_rpc wish_app wish_app_deps port/unix deps/bson/ deps/uthash/include mist
+LOCAL_SRC_FILES := wish_bridge_jni.c jni_utils.c wish_core_api.c mist_node_api_jni.c mist-c99/wish_app_deps/wish_platform.c mist-c99/wish_app_deps/wish_debug.c mist-c99/wish_app_deps/wish_utils.c mist-c99/wish_app_deps/wish_fs.c filesystem.c $(foreach sdir,$(WISH_MODULES),$(wildcard $(sdir)/*.c))
+LOCAL_C_INCLUDES := mist-c99/deps/mbedtls-2.1.2/include mist-c99/deps/cbson/src mist-c99/wish mist-c99/deps/ed25519/src mist-c99/deps/wish-rpc-c99/src mist-c99/wish_app mist-c99/wish_app_deps mist-c99/port/unix mist-c99/deps/bson/ mist-c99/deps/uthash/include mist-c99/mist
 LOCAL_LDLIBS := -llog
-LOCAL_CFLAGS := -O2 -Wall -Wno-pointer-sign -Werror -fvisibility=hidden -Wno-unused-variable -Wno-unused-function
+LOCAL_CFLAGS := -g -O0 -Wall -Wno-pointer-sign -Werror -fvisibility=hidden -Wno-unused-variable -Wno-unused-function
 
 #Put each function in own section, so that linker can discard unused code
 LOCAL_CFLAGS += -ffunction-sections -fdata-sections 
