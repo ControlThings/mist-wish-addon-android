695d5685780435776976e6e88f473fb63b63e8e5
Jesper Lökfors <jesper.lokfors@controlthings.fi>
2017-10-24 12:39:40 +0300

    controll follow

--- MistNodeApi/src/main/java/mist/request/Control.java
+++ MistNodeApi/src/main/java/mist/request/Control.java
@@ -17,6 +17,10 @@ public class Control {
         return ControlModel.request(peer, callback);
     };
 
+    public static int read(Peer peer, String epid, Control.ReadCb callback) {
+       return ControlRead.request(peer, epid, callback);
+    }
+
     public static int write(Peer peer, String epid, Boolean state, Control.WriteCb callback) {
         return ControlWrite.request(peer, epid, state, callback);
     }
@@ -30,21 +34,16 @@ public class Control {
         return ControlWrite.request(peer, epid, state, callback);
     }
 
-    public static int read(Peer peer, String epid, Control.ReadCb callback) {
-       return ControlRead.request(peer, epid, callback);
-    }
-
     public static int invoke(Peer peer, String epid, byte[] bson, InvokeCb callback) {
         return ControlInvoke.request(peer, epid, bson, callback);
     }
 
+    public static int follow(Peer peer, FollowCb callback) {
+        return ControlFollow.request(peer, callback);
+    }
 
     public abstract static class ModelCb extends Callback {
         public abstract void cb(BsonDocument data);
-    }
-
-    public abstract static class WriteCb extends Callback {
-        public abstract void cb();
     }
 
     public abstract static class ReadCb extends Callback {
@@ -54,7 +53,18 @@ public class Control {
         public void cbString(String data) {};
     }
 
+    public abstract static class WriteCb extends Callback {
+        public abstract void cb();
+    }
+
     public abstract static class InvokeCb extends Callback {
         public abstract void cb(byte[] bson);
+    }
+
+    public abstract static class FollowCb extends Callback {
+        public void cbBool(String epid, boolean value) {};
+        public void cbInt(String epid, int value) {};
+        public void cbFloat(String epid, float value) {};
+        public void cbString(String epid, String value) {};
     }
 }
