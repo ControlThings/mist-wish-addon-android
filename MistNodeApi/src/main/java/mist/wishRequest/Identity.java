9f90a1cf04115de22fada8ed9af57df4756b98cd
Jesper Lökfors <jesper.lokfors@controlthings.fi>
2017-10-25 13:50:26 +0300

    identity list added

--- MistNodeApi/src/main/java/mist/wishRequest/Identity.java
+++ MistNodeApi/src/main/java/mist/wishRequest/Identity.java
@@ -1,6 +1,7 @@
 package mist.wishRequest;
 
 
+import java.util.List;
 
 /**
  * Created by jeppe on 10/24/17.
@@ -8,9 +9,18 @@ package mist.wishRequest;
 
 public class Identity {
 
-    public static int get(byte[] uid, GetCb callback){
+    public static int list(ListCb callback) {
+        return IdentityList.request(callback);
+    }
+
+    public static int get(byte[] uid, GetCb callback) {
         return IdentityGet.request(uid, callback);
     };
+
+
+    public abstract static class ListCb extends Callback {
+        public abstract void cb (List<mist.Identity> identities);
+    }
 
     public abstract static class GetCb extends Callback {
         public abstract void cb (mist.Identity identity);
---
+++ MistNodeApi/src/main/java/mist/wishRequest/IdentityList.java
@@ -0,0 +1,71 @@
+package mist.wishRequest;
+
+import org.bson.BSONException;
+import org.bson.BsonArray;
+import org.bson.BsonBinaryWriter;
+import org.bson.BsonDocument;
+import org.bson.BsonValue;
+import org.bson.BsonWriter;
+import org.bson.RawBsonDocument;
+import org.bson.io.BasicOutputBuffer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mist.node.MistNode;
+
+class IdentityList {
+    static int request(Identity.ListCb callback) {
+        String op = "identity.list";
+
+
+        BasicOutputBuffer buffer = new BasicOutputBuffer();
+        BsonWriter writer = new BsonBinaryWriter(buffer);
+        writer.writeStartDocument();
+
+        writer.writeString("op", op);
+
+        writer.writeStartArray("args");
+        writer.writeEndArray();
+
+        writer.writeInt32("id", 0);
+
+        writer.writeEndDocument();
+        writer.flush();
+
+        return MistNode.getInstance().wishRequest(buffer.toByteArray(), new MistNode.RequestCb() {
+            Identity.ListCb cb;
+
+            @Override
+            public void response(byte[] data) {
+                try {
+                    BsonDocument bson = new RawBsonDocument(data);
+                    BsonArray bsonList = bson.getArray("data");
+                    List<mist.Identity> list = new ArrayList<mist.Identity>();
+                    for (BsonValue bsonIdentity : bsonList) {
+                        list.add(mist.Identity.fromBson(bsonIdentity.asDocument()));
+                    }
+                    cb.cb(list);
+                } catch (BSONException e) {
+                    cb.err(Callback.BSON_ERROR_CODE, Callback.BSON_ERROR_STRING);
+                }
+            }
+
+            @Override
+            public void end() {
+                cb.end();
+            }
+
+            @Override
+            public void err(int code, String msg) {
+                cb.err(code, msg);
+            }
+
+            private MistNode.RequestCb init(Identity.ListCb callback) {
+                this.cb = callback;
+                return this;
+            }
+
+        }.init(callback));
+    }
+}
