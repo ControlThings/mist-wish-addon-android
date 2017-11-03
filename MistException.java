ef658fea39b508641a93725572c498df059fc083
Jesper Lökfors <jesper.lokfors@controlthings.fi>
2017-11-03 13:34:40 +0200

    added addon from mist-node-android

---
+++ MistException.java
@@ -0,0 +1,11 @@
+package addon;
+
+/**
+ * Created by jan on 10/19/17.
+ */
+
+public class MistException extends RuntimeException {
+    public MistException(String s) {
+        super(s);
+    }
+}
---
+++ MistReceiver.java
@@ -0,0 +1,36 @@
+package addon;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+
+/**
+ * Created by jeppe on 10/25/17.
+ */
+
+public class MistReceiver extends ResultReceiver {
+
+    private Receiver mReceiver;
+
+    public MistReceiver(Receiver receiver) {
+        super(new Handler());
+        mReceiver = receiver;
+        //super();
+    }
+
+    public interface Receiver {
+        public void onConnected();
+    }
+
+    @Override
+    protected void onReceiveResult(int resultCode, Bundle resultData) {
+        if (mReceiver != null) {
+            if (resultCode == WishBridgeJni.BRIDGE_CONNECTED) {
+                mReceiver.onConnected();
+            }
+        }
+    }
+
+
+
+}
